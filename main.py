2fc3f60862f257f74bd493230f70a3e439f0918b
eadlpl <eadlpl@localhost.localdomain>
2021-02-18 14:00:04 -0700

    Integracion_href

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html",logo='/home/eadlpl/Documents/web/DetlosSecurity/templates/images/logo.png')
 
 if __name__ == '__main__':
     app.run(debug=True)
