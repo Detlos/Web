65078c18b3a13a17c0dce98d64ca442181c89509
eadlpl <eadlpl@localhost.localdomain>
2021-02-18 16:55:45 -0700

    navbarlogo

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from flask import Flask, redirect, render_template,url_for,request,flash
-<<<<<<< HEAD
-=======
 import requests as api
->>>>>>> 2fc3f60862f257f74bd493230f70a3e439f0918b
 import json
 import os
 
@@ -13,7 +10,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-<<<<<<< HEAD
     return render_template("index.html")
 
 @app.route('/register')
@@ -22,9 +18,3 @@ def register():
 
 if __name__ == '__main__':
     app.run(port = 5000,debug=True)
-=======
-    return render_template("index.html",logo='/home/eadlpl/Documents/web/DetlosSecurity/templates/images/logo.png')
-
-if __name__ == '__main__':
-    app.run(debug=True)
->>>>>>> 2fc3f60862f257f74bd493230f70a3e439f0918b
