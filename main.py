c40974605ef9a9e82995c4149fb1e6d1254d0406
TheMakeer <sipkicks@gmail.com>
2021-02-20 13:52:49 -0700

    rick login

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from flask import Flask, redirect, render_template,url_for,request,flash
-<<<<<<< HEAD
-=======
-import requests as api
->>>>>>> 2fc3f60862f257f74bd493230f70a3e439f0918b
 import json
 import os
 
@@ -13,18 +9,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-<<<<<<< HEAD
     return render_template("index.html")
 
 @app.route('/register')
 def register():
     return render_template("register.html")
 
-if __name__ == '__main__':
-    app.run(port = 5000,debug=True)
-=======
-    return render_template("index.html",logo='/home/eadlpl/Documents/web/DetlosSecurity/templates/images/logo.png')
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
->>>>>>> 2fc3f60862f257f74bd493230f70a3e439f0918b
+    app.run(port = 5000,debug=True)
