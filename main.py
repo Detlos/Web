10d8b2733a8f3bec3e2aec6d9c3be1adaa7cc08b
TheMakeer <sipkicks@gmail.com>
2021-02-23 20:04:01 -0700

    login

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from flask import Flask, redirect, render_template,url_for,request,flash
+from flask.globals import session
+import pymongo
+from pymongo import MongoClient
 import requests as api
 import json
 import os
 
 # --
+client = pymongo.MongoClient("mongodb+srv://eadlpl11:5PinS5Jvi5WdHOOA@cluster0.dnnfi.mongodb.net/test?retryWrites=true&w=majority")
+
+
+db = client.test
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html",)
 
 @app.route('/perfil')
 def perfil():
@@ -30,9 +37,26 @@ def video():
 def register():
     return render_template("register.html")
 
-@app.route('/login')
+@app.route('/login', methods = ['GET','POST'])
 def login():
+    users = db.camaras.find({'email':request.form.get('correo')})
+    password = db.camaras.find({'correo':request.form.get('pass')})
+    if request.method == 'POST':
+        correo = request.form.get('correo')
+        password = request.form.get('pass')
+        error = None
+
+        if users is None or password is None:
+            error = 'Contraseña o correo invalidos'
+        
+        if error is None:
+            return redirect(url_for('home')) 
+
+        flash(error)
+
     return render_template("login.html")
+
+
 
 @app.route('/verification_register', methods=['POST','GET'])
 def verRegister():
