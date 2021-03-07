548c8fb92b485b7d9066ba0bdaac584bd2a82982
eadlpl11 <eadlpl11@gmail.com>
2021-03-06 19:31:59 -0700

    quitar venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/nword/Documents/Escuela/Web/venv"
+set -gx VIRTUAL_ENV "/home/eadlpl/Documents/web/Web/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
